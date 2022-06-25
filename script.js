86459a1091f6e9b4acb6e4fc9bcc92836edafa32
philip-hru <philip.huz11@gmail.com>
2022-06-25 22:52:39 +0300

    сделал что бы свинка после порожения не могла двигаться сделал при порожения что бы свинка падала сделал натпись при победе

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var pig_1_move_right;
 var pig_2_move_left;
 var pig_2_move_right;
 function punch_1(event) {
-  if (event.key == "s") {
+  if (event.key == "s" && pig_1_lifes > 0) {
     document.getElementById("span_1").style.display = "inline";
     window.setTimeout(punch_1_hide, 250);
     if (check_collision() == true) {
@@ -22,7 +22,7 @@ function punch_1_hide() {
 }
 
 function punch_2(event) {
-  if (event.key == ";") {
+  if (event.key == ";" && pig_2_lifes > 0) {
     document.getElementById("span_2").style.display = "inline";
     window.setTimeout(punch_2_hide, 250);
     if (check_collision() == true) {
@@ -35,24 +35,17 @@ function punch_2(event) {
 function punch_2_hide() {
   document.getElementById("span_2").style.display = "none";
 }
-
-function move_pig_1_1(event) {
-  if (event.key == "a") {
-    pig_1_position = pig_1_position - 5;
-    document.getElementById("pig_1").style.left = pig_1_position + "px";
-  }
-}
 function move_pig(event) {
-  if (event.key == "a") {
+  if (event.key == "a" && pig_1_lifes > 0) {
     pig_1_move_left = true;
   }
-  if (event.key == "d") {
+  if (event.key == "d" && pig_1_lifes > 0) {
     pig_1_move_right = true;
   }
-  if (event.key == "l") {
+  if (event.key == "l" && pig_2_lifes > 0) {
     pig_2_move_left = true;
   }
-  if (event.key == "'") {
+  if (event.key == "'" && pig_2_lifes > 0) {
     pig_2_move_right = true;
   }
 }
@@ -70,22 +63,19 @@ function stop_pig(event) {
     pig_2_move_right = false;
   }
 }
-function move_pig_1_2(event) {
-  if (event.key == "d") {
-    pig_1_position = pig_1_position + 5;
-    document.getElementById("pig_1").style.left = pig_1_position + "px";
+
+function check_win() {
+  if (pig_2_lifes == 0) {
+    document.getElementById("win_text").style.display = "block";
+    document.getElementById("win_text").innerHTML = "pig 1 win";
+    document.getElementById("pig_2").style.transform =
+      "translateY(-100%) rotate(90deg)";
   }
-}
-function move_pig_2_1(event) {
-  if (event.key == "l") {
-    pig_2_position = pig_2_position + 5;
-    document.getElementById("pig_2").style.right = pig_2_position + "px";
-  }
-}
-function move_pig_2_2(event) {
-  if (event.key == "'") {
-    pig_2_position = pig_2_position - 5;
-    document.getElementById("pig_2").style.right = pig_2_position + "px";
+  if (pig_1_lifes == 0) {
+    document.getElementById("win_text").style.display = "block";
+    document.getElementById("win_text").innerHTML = "pig 2 win";
+    document.getElementById("pig_1").style.transform =
+      "translateY(-100%) rotate(270deg)";
   }
 }
 
@@ -97,6 +87,7 @@ function check_collision() {
   }
 }
 function repeat() {
+  check_win();
   if (pig_1_move_left) {
     pig_1_position = pig_1_position = pig_1_position - 5;
     document.getElementById("pig_1").style.left = pig_1_position + "px";
@@ -115,8 +106,36 @@ function repeat() {
   }
 }
 
-document.addEventListener("keypress", punch_1);
-document.addEventListener("keypress", punch_2);
+function throttle(f, t) {
+  return function (args) {
+    let previousCall = this.lastCall;
+    this.lastCall = Date.now();
+    if (
+      previousCall === undefined || // function is being called for the first time
+      this.lastCall - previousCall > t
+    ) {
+      // throttle time has elapsed
+      f(args);
+    }
+  };
+}
+
+function throttle_1(r, s) {
+  return function (args_1) {
+    let previousCall = this.lastCall_1;
+    this.lastCall_1 = Date.now();
+    if (
+      previousCall === undefined || // function is being called for the first time
+      this.lastCall_1 - previousCall > s
+    ) {
+      // throttle time has elapsed
+      r(args_1);
+    }
+  };
+}
+
+document.addEventListener("keydown", throttle(punch_1, 250));
+document.addEventListener("keypress", throttle_1(punch_2, 250));
 document.addEventListener("keydown", move_pig);
 document.addEventListener("keyup", stop_pig);
 window.setInterval(repeat, 100);
